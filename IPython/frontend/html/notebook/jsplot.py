8badf7e1d88cadf232a596e5382caf815d0148c3
wilsaj <wilson.andrew.j+github@gmail.com>
2012-02-20 14:17:15 -0600

    add a context manager for plotting

--- IPython/frontend/html/notebook/jsplot.py
+++ IPython/frontend/html/notebook/jsplot.py
@@ -1,6 +1,8 @@
 """experimental javascript plotting
 """
-from IPython.core.display import Javascript
+import sys
+
+from IPython.core.display import display, Javascript
 
 try:
     import simplejson as json
@@ -11,3 +13,25 @@ except ImportError:
 def plot(x, y):
     points = [zip(x, y)]
     return Javascript("new IPython.Plot(element, %s);" % json.dumps(points))
+
+
+def line(x, y):
+    plot_ctx = current_plot_context()
+    plot_ctx.points.append(zip(x, y))
+
+
+class plot_context:
+    def __enter__(self):
+        plot_ctx = current_plot_context()
+        plot_ctx.points = []
+
+    def __exit__(self, type, value, traceback):
+        plot_ctx = current_plot_context()
+        js = Javascript("new IPython.Plot(element, %s);" % json.dumps(
+            plot_ctx.points))
+        display(js)
+
+
+def current_plot_context():
+    """returns the current plot context"""
+    return sys.modules[__name__]
